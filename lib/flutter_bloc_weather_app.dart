901b1d536e0e47e0663d420473785815e9b53be5
cromueloliver02 <cromuelbarut@gmail.com>
2022-12-12 23:53:25 +0800

    init app

---
+++ lib/flutter_bloc_weather_app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FlutterBlocWeatherApp extends StatelessWidget {
+  const FlutterBlocWeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Bloc Weather',
+      theme: ThemeData.light(),
+      home: const Scaffold(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+import './flutter_bloc_weather_app.dart';
+
+void main() => runApp(const FlutterBlocWeatherApp());
